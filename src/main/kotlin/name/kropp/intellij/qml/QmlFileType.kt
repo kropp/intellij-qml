cc1908f9c0ef4941eefaa1ae2fdc86f18c1d6100
Victor Kropp <victor.kropp@gmail.com>
2017-03-02 23:08:12 +0100

    QML file type

---
+++ src/main/kotlin/name/kropp/intellij/qml/QmlFileType.kt
@@ -0,0 +1,21 @@
+package name.kropp.intellij.qml
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.fileTypes.FileTypeConsumer
+import com.intellij.openapi.fileTypes.FileTypeFactory
+import com.intellij.openapi.fileTypes.LanguageFileType
+
+val QmlIcon = AllIcons.FileTypes.UiForm!!
+
+object QmlFileType : LanguageFileType(QmlLanguage) {
+  override fun getIcon() = QmlIcon
+  override fun getName() = "QML"
+  override fun getDescription() = "Qt User Interface Markup Language"
+  override fun getDefaultExtension() = "qml"
+}
+
+object QmlFileTypeFactory : FileTypeFactory() {
+  override fun createFileTypes(consumer: FileTypeConsumer) {
+    consumer.consume(QmlFileType)
+  }
+}
