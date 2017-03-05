c0eae48bb25b29410927e6fea5e2289e76a3b51e
Victor Kropp <victor.kropp@gmail.com>
2017-03-05 22:30:47 +0100

    import folding

--- src/main/kotlin/name/kropp/intellij/qml/QmlFoldingBuilder.kt
+++ src/main/kotlin/name/kropp/intellij/qml/QmlFoldingBuilder.kt
@@ -7,15 +7,27 @@ import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import name.kropp.intellij.qml.psi.QmlBody
+import name.kropp.intellij.qml.psi.QmlImports
 
 class QmlFoldingBuilder : FoldingBuilderEx() {
   override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor>
-      = PsiTreeUtil.findChildrenOfAnyType(root, QmlBody::class.java)
-      .mapNotNull(::QmlBodyFoldingDescriptor)
+      = PsiTreeUtil.findChildrenOfAnyType(root, QmlImports::class.java, QmlBody::class.java)
+      .mapNotNull {
+        when (it) {
+          is QmlImports -> QmlImportsFoldingDescriptor(it)
+          is QmlBody -> QmlBodyFoldingDescriptor(it)
+          else -> null
+        }
+      }
       .toTypedArray()
 
   override fun getPlaceholderText(node: ASTNode) = "{}"
   override fun isCollapsedByDefault(node: ASTNode) = false
 
   class QmlBodyFoldingDescriptor(body: QmlBody) : FoldingDescriptor(body, body.textRange)
+  class QmlImportsFoldingDescriptor(imports: QmlImports) : FoldingDescriptor(imports, imports.textRange) {
+    override fun getPlaceholderText(): String? {
+      return "import..."
+    }
+  }
 }
