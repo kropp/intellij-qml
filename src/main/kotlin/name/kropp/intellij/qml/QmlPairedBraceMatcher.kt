f26ec5372361eeebe75f00e925d0fc658016750b
Victor Kropp <victor.kropp@gmail.com>
2017-03-05 22:03:48 +0100

    paired brace matcher

---
+++ src/main/kotlin/name/kropp/intellij/qml/QmlPairedBraceMatcher.kt
@@ -0,0 +1,22 @@
+package name.kropp.intellij.qml
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import name.kropp.intellij.qml.psi.QmlObject
+import name.kropp.intellij.qml.psi.QmlTypes
+
+class QmlPairedBraceMatcher : PairedBraceMatcher {
+  override fun getPairs() = arrayOf(BracePair(QmlTypes.LBRACE, QmlTypes.RBRACE, true))
+  override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+
+  override fun getCodeConstructStart(file: PsiFile, openingBraceOffset: Int): Int {
+    val element = file.findElementAt(openingBraceOffset)
+    val obj = element?.parent
+    if (obj is QmlObject) {
+      return obj.textRange.startOffset
+    }
+    return openingBraceOffset
+  }
+}
