d9330f3410b37d426ad1a0b8f2b479b40db6f170
Victor Kropp <victor.kropp@gmail.com>
2017-03-19 14:54:28 +0100

    handle import rename correctly

--- src/main/kotlin/name/kropp/intellij/qml/psi/QmlFilenameManipulator.kt
+++ src/main/kotlin/name/kropp/intellij/qml/psi/QmlFilenameManipulator.kt
@@ -5,7 +5,9 @@ import com.intellij.psi.ElementManipulator
 
 class QmlFilenameManipulator : ElementManipulator<QmlFilename> {
   override fun handleContentChange(element: QmlFilename, range: TextRange, newContent: String?): QmlFilename {
-    return element
+    val oldText = element.text
+    val newText = oldText.substring(0, range.startOffset) + newContent + oldText.substring(range.endOffset, oldText.length)
+    return element.updateText(newText)
   }
 
   override fun handleContentChange(element: QmlFilename, newContent: String?) = handleContentChange(element, getRangeInElement(element), newContent)
