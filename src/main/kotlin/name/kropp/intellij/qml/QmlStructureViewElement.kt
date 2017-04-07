b19aca94e83098187e7a233588ff969470098ebb
Victor Kropp <victor.kropp@gmail.com>
2017-04-07 18:05:02 +0200

    navigation from file structure

--- src/main/kotlin/name/kropp/intellij/qml/QmlStructureViewElement.kt
+++ src/main/kotlin/name/kropp/intellij/qml/QmlStructureViewElement.kt
@@ -2,6 +2,7 @@ package name.kropp.intellij.qml
 
 import com.intellij.ide.structureView.StructureViewTreeElement
 import com.intellij.ide.util.treeView.smartTree.TreeElement
+import com.intellij.navigation.NavigationItem
 import name.kropp.intellij.qml.psi.QmlPsiElement
 
 class QmlStructureViewElement(private val element: QmlPsiElement) : StructureViewTreeElement {
@@ -11,20 +12,12 @@ class QmlStructureViewElement(private val element: QmlPsiElement) : StructureVie
     return element.children.filterIsInstance<QmlPsiElement>().map(::QmlStructureViewElement).toTypedArray()
   }
 
-  override fun canNavigate(): Boolean {
-    return false
-  }
+  override fun getValue() = element
 
-  override fun getValue(): Any {
-    return element
-  }
-
-  override fun canNavigateToSource(): Boolean {
-    return false
-  }
+  override fun canNavigate() = (element as? NavigationItem)?.canNavigate() == true
+  override fun canNavigateToSource() = (element as? NavigationItem)?.canNavigateToSource() == true
 
   override fun navigate(requestFocus: Boolean) {
-
+    (element as? NavigationItem)?.navigate(requestFocus)
   }
-
 }
